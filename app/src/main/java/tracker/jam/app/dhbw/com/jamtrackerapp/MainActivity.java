9ad99d2a8da772d38808a680adebeb9b0a0d99e4
Christian Vogel <SignPhoenixCris@web.de>
2018-02-20 23:16:29 +0100

    Create empty app

---
+++ app/src/main/java/tracker/jam/app/dhbw/com/jamtrackerapp/MainActivity.java
@@ -0,0 +1,13 @@
+package tracker.jam.app.dhbw.com.jamtrackerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
