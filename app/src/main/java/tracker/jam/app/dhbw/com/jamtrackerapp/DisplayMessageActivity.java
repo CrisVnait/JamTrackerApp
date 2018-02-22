c42b04612ceca8ef6ae690f26d0757190d233188
Christian Vogel <SignPhoenixCris@web.de>
2018-02-22 13:46:07 +0100

    Add network request (volley)

--- app/src/main/java/tracker/jam/app/dhbw/com/jamtrackerapp/DisplayMessageActivity.java
+++ app/src/main/java/tracker/jam/app/dhbw/com/jamtrackerapp/DisplayMessageActivity.java
@@ -1,9 +1,20 @@
 package tracker.jam.app.dhbw.com.jamtrackerapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.logging.Logger;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -12,11 +23,29 @@ public class DisplayMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
-        // Get the Intent that started this activity
-        Intent intent = getIntent();
+        final TextView textView = findViewById(R.id.textView);
+        String url = "https://faketrafficinformation.azurewebsites.net/api/FakeTrafficInformationFunction";
 
-        // Capture the layout's TextView and set text
-        TextView textView = findViewById(R.id.textView);
-        textView.setText("Test2");
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            textView.setText(new JSONObject(response).getJSONObject("density").toString());
+                        } catch (JSONException e) {
+                            textView.setText("parse error");
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("request error");
+                    }
+                });
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(stringRequest);
+
     }
 }
