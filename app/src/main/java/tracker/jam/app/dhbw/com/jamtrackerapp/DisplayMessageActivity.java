92910400a5624ad85dba49314bf8889c89dd2909
Christian Vogel <SignPhoenixCris@web.de>
2018-02-27 09:24:43 +0100

    Use other json dummy url

--- app/src/main/java/tracker/jam/app/dhbw/com/jamtrackerapp/DisplayMessageActivity.java
+++ app/src/main/java/tracker/jam/app/dhbw/com/jamtrackerapp/DisplayMessageActivity.java
@@ -14,8 +14,6 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.logging.Logger;
-
 public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
@@ -24,15 +22,14 @@ public class DisplayMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_message);
 
         final TextView textView = findViewById(R.id.textView);
-        String url = "https://faketrafficinformation.azurewebsites.net/api/FakeTrafficInformationFunction";
+        String url = "http://my-json-server.typicode.com/crisvnait/jsondummy/test";
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
-                            String jsonString = response.substring(1, response.length()-1).replace("\\", "");
-                            textView.setText(String.valueOf(new JSONObject(jsonString).getInt("density")));
+                            textView.setText(String.valueOf(new JSONObject(response).getInt("eins")));
                         } catch (JSONException e) {
                             textView.setText("parse error");
                         }
